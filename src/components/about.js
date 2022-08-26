54058b112ed13c3392ddb1d3cc32d6cb2d82dbe2
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-25 20:24:12 -0700

    aboutme

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+const AboutMe = () => {
+    return (
+     <div class="d-flex justify-content-center flex-wrap">
+         <h1 class= "col-12 justify-content-center d-flex"> About Me</h1>
+         <div class="d-flex justify-content-center">
+         <img src="coolpicthatineedtoinsert"alt=""></img>
+</div>
+   <p> Hello my name is Yahir, I am from San Diego, California. I am very passionate about Computer Science and strive to continue improving my knowledge. 
+    With the skills I have developed through UC Berkeley BootCamp I am be able to take on Complex level tasks that require the use of multiple technologies.
+    These technologies include but are not limited to Js, React, Handlebars and many more technologies I have learned as a full stack Web Developer. Such Skills that can be displayed in Real-world applications. My portfolio is here to provide
+    insight as to the depth of my abilities with demonstrations of different technologies I have used.
+    For any inquiries Feel free to contact me through my contact information down below.
+</p>
+</div>
+    )
+}
+ export default AboutMe;
